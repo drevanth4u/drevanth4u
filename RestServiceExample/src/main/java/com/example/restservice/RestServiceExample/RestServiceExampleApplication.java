515c3737b9eec096dae64778b4d94381233da284
drevanth4u <46389696+drevanth4u@users.noreply.github.com>
2021-05-19 02:55:00 -0500

    Add files via upload

---
+++ RestServiceExample/src/main/java/com/example/restservice/RestServiceExample/RestServiceExampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.restservice.RestServiceExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceExampleApplication.class, args);
+	}
+
+}
+
